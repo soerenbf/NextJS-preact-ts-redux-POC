eb48d030f12e87a6d36f01f613c9512515446f62
Søren Bruus Zeppelin <soerenbf@gmail.com>
2020-03-26 10:13:53 +0100

    pages + more preact

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    webpack: function (config) {
+        config.resolve.alias = {
+            'react': 'preact/compat',
+            'react-dom': 'preact/compat'
+        }
+
+        return config
+    }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Index() {
+    return (
+        <div>
+            <Link href="/about">About page</Link>
+            <p>Hello Next.js</p>
+        </div>
+    );
+}
