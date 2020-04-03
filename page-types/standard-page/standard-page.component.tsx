094c2ab08e2e4aa9dedb0bc014a3357f5bb3653e
Søren Bruus Zeppelin <soerenbf@gmail.com>
2020-04-03 08:01:43 +0200

    style example

--- page-types/standard-page/standard-page.component.tsx
+++ page-types/standard-page/standard-page.component.tsx
@@ -4,6 +4,6 @@ import styles from "./standard-page.module.scss";
 
 export const StandardPage: FC = () => {
     return (
-        <div className={styles.content}>Standard page</div>
+        <div className={`${styles.content} ${styles.test} ${styles["test--mod"]}`}>Standard page</div>
     );
 }
